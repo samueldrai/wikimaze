dbca90c8461c9260b7a460a522891f4d2c9fba15
samueldrai <drai.samuel@gmail.com>
2020-12-16 14:20:50 +0100

    style(sidebar): polish design

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,28 +1,49 @@
 import React from "react"
 
-const Sidebar = ({ players }) => (
+const Sidebar = ({ players, wikiPage }) => (
   <div className="flex h-screen overflow-hidden bg-white">
     <div className="hidden md:flex md:flex-shrink-0">
-      <div className="flex flex-col w-64">
+      <div className="flex flex-col w-80">
         <div className="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto border-r border-gray-200">
           <div className="flex items-center flex-shrink-0 px-4">
             <span className="font-bold uppercase">Wikimaze</span>
           </div>
           <div className="flex flex-col flex-grow mt-5">
             <nav className="flex-1 px-2 space-y-1 bg-white">
-              <h3 className="font-extrabold">Players</h3>
-              <div className="flex flex-col px-2">
-                {players.map(player => (
-                  <span className="font-light">{player}</span>
-                ))}
+              <div>
+                <h3 className="font-extrabold">Players</h3>
+                <div className="flex flex-col px-2">
+                  {players.map(player => (
+                    <span className="font-light">{player}</span>
+                  ))}
+                </div>
               </div>
+              <div className="mt-2">
+                <h3 className="font-extrabold">Invite your friends</h3>
+                <div className="mt-1 mb-4">
+                  <input
+                    type="text"
+                    name="gameUrl"
+                    id="gameUrl"
+                    className="block w-full transition duration-300 ease-in-out border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    placeholder="https://wikimaze.io/1234"
+                    value="https://wikimaze.io/1234"
+                  />
+                </div>
+              </div>
+              <button
+                type="button"
+                className="px-4 py-2 mt-2 text-sm font-medium text-center text-white transition duration-300 ease-in-out bg-indigo-600 border border-transparent rounded-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Start Game!
+              </button>
             </nav>
           </div>
         </div>
       </div>
     </div>
-    <div className="flex flex-col flex-1 w-0 max-w-4xl mx-auto md:px-8 xl:px-0">
-      <div className="relative z-10 flex flex-shrink-0 h-16 bg-white border-b border-gray-200">
+    <div className="flex flex-col flex-1 w-0 mx-auto md:px-8 xl:px-0">
+      <div className="relative z-10 flex flex-shrink-0 h-16 px-4 bg-white border-b border-gray-200">
         <div className="flex justify-between flex-1 px-4 md:px-0">
           <div className="flex flex-1">
             <form className="flex w-full md:ml-0" action="#" method="GET">
@@ -49,8 +70,10 @@ const Sidebar = ({ players }) => (
                   id="search_field"
                   className="block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
                   placeholder="Search"
-                  type="search"
+                  type="text"
+                  value="https://wikipedia.com/fr/barack_obama"
                   name="search"
+                  disabled
                 />
               </div>
             </form>
@@ -58,19 +81,9 @@ const Sidebar = ({ players }) => (
         </div>
       </div>
 
-      <main
-        className="relative flex-1 overflow-y-auto focus:outline-none"
-        tabindex="0"
-      >
+      <main className="relative flex-1">
         <div className="py-6">
-          <div className="px-4 sm:px-6 md:px-0">
-            <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-          </div>
-          <div className="px-4 sm:px-6 md:px-0">
-            <div className="py-4">
-              <div className="border-4 border-gray-200 border-dashed rounded-lg h-96"></div>
-            </div>
-          </div>
+          <div className="px-4 sm:px-6 md:px-0"></div>
         </div>
       </main>
     </div>
