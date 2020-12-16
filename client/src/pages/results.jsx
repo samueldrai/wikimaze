7d0b040d957c01a01b8da6c2c96b8cc3f18720bc
Nicolas Spehler <nicolas@spehler.com>
2020-12-16 19:51:49 +0400

    Add results page

--- client/src/pages/results.jsx
+++ client/src/pages/results.jsx
@@ -61,47 +61,45 @@ const Results = ({ ...props }) => {
 
   return (
     <>
-      <div className="pt-12 bg-gray-50 sm:pt-16">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <section className="bg-gray-50 sm:pt-16 pt-12">
+        <div className="max-w-7xl sm:px-6 lg:px-8 px-4 mx-auto">
           <div className="max-w-4xl mx-auto text-center">
-            <Trophy className="text-gray-500 w-12 h-12" />
-            <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-              {winner.username.charAt(0).toUpperCase() +
-                winner.username.slice(1)}{" "}
-              won!
+            <Trophy className="w-16 h-16 mx-auto mb-3 text-gray-900 fill-current" />
+            <h2 className="sm:text-4xl text-3xl font-extrabold text-gray-900">
+              Thomas won!
             </h2>
-            <p className="mt-3 text-xl text-gray-500 sm:mt-4">
+            <p className="sm:mt-4 mt-3 text-xl text-gray-500">
               Nice try, better luck next time!
             </p>
           </div>
         </div>
-        <div className="pb-12 mt-10 bg-white sm:pb-16">
+        <div className="sm:pb-16 pb-12 mt-10 bg-white">
           <div className="relative">
-            <div className="absolute inset-0 h-1/2 bg-gray-50"></div>
-            <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="h-1/2 bg-gray-50 absolute inset-0"></div>
+            <div className="max-w-7xl sm:px-6 lg:px-8 relative px-4 mx-auto">
               <div className="max-w-4xl mx-auto">
-                <dl className="bg-white rounded-lg shadow-lg sm:grid sm:grid-cols-3">
-                  <div className="flex flex-col p-6 text-center border-b border-gray-100 sm:border-0 sm:border-r">
+                <dl className="sm:grid sm:grid-cols-3 bg-white rounded-lg shadow-lg">
+                  <div className="sm:border-0 sm:border-r flex flex-col p-6 text-center border-b border-gray-100">
                     <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                       pages visited
                     </dt>
-                    <dd className="order-1 text-5xl font-extrabold text-indigo-600">
+                    <dd className="order-1 text-5xl font-extrabold text-blue-500">
                       {winner.pages.length}
                     </dd>
                   </div>
-                  <div className="flex flex-col p-6 text-center border-t border-b border-gray-100 sm:border-0 sm:border-l sm:border-r">
+                  <div className="sm:border-0 sm:border-l sm:border-r flex flex-col p-6 text-center border-t border-b border-gray-100">
                     <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                       avg. seconds/page
                     </dt>
-                    <dd className="order-1 text-5xl font-extrabold text-indigo-600">
+                    <dd className="order-1 text-5xl font-extrabold text-blue-500">
                       3.6
                     </dd>
                   </div>
-                  <div className="flex flex-col p-6 text-center border-t border-gray-100 sm:border-0 sm:border-l">
+                  <div className="sm:border-0 sm:border-l flex flex-col p-6 text-center border-t border-gray-100">
                     <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                       total seconds
                     </dt>
-                    <dd className="order-1 text-5xl font-extrabold text-indigo-600">
+                    <dd className="order-1 text-5xl font-extrabold text-blue-500">
                       20.4
                     </dd>
                   </div>
@@ -110,7 +108,125 @@ const Results = ({ ...props }) => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="sm:px-4 lg:px-8 container px-4 mx-auto">
+        <div className="mb-4">
+          <div className="sm:hidden">
+            <label for="tabs" className="sr-only">
+              Select a player
+            </label>
+            <select
+              id="tabs"
+              name="tabs"
+              className="focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md"
+            >
+              <option>Thomas</option>
+              <option>Sarah</option>
+              <option selected>Sam</option>
+              <option>Ezio</option>
+              <option>Nicolas</option>
+            </select>
+          </div>
+          <div className="sm:block hidden">
+            <nav className="flex space-x-4" aria-label="Tabs">
+              <a
+                href="#"
+                className="hover:text-gray-700 px-3 py-2 text-sm font-medium text-gray-500 rounded-md"
+              >
+                Thomas
+              </a>
+              <a
+                href="#"
+                className="hover:text-gray-700 px-3 py-2 text-sm font-medium text-gray-500 rounded-md"
+              >
+                Sarah
+              </a>
+              <a
+                href="#"
+                className="px-3 py-2 text-sm font-medium text-blue-500 bg-blue-100 rounded-md"
+                aria-current="page"
+              >
+                Sam
+              </a>
+              <a
+                href="#"
+                className="hover:text-gray-700 px-3 py-2 text-sm font-medium text-gray-500 rounded-md"
+              >
+                Ezio
+              </a>
+              <a
+                href="#"
+                className="hover:text-gray-700 px-3 py-2 text-sm font-medium text-gray-500 rounded-md"
+              >
+                Nicolas
+              </a>
+            </nav>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="sm:-mx-6 lg:-mx-8 -my-2 overflow-x-auto">
+            <div className="sm:px-6 lg:px-8 inline-block min-w-full py-2 align-middle">
+              <div className="sm:rounded-lg overflow-hidden border-b border-gray-200 shadow">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                      >
+                        Page
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                      >
+                        Time
+                      </th>
+                      <th scope="col" className="relative px-6 py-3">
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="bg-white">
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
+                        Kamala Harris
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                        4.4s
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-right">
+                        <a
+                          href="#"
+                          className="hover:text-blue-900 text-blue-600"
+                        >
+                          View
+                        </a>
+                      </td>
+                    </tr>
+                    <tr className="bg-gray-50">
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
+                        U.S. Vice President
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                        3.2s
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-right">
+                        <a
+                          href="#"
+                          className="hover:text-blue-900 text-blue-600"
+                        >
+                          View
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
